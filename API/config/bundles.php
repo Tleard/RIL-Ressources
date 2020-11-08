2cce4c56d37dbabeec86da9e798768d24fcd238e
Thomas <thomas.leard@gmail.com>
2020-11-08 16:31:36 +0100

    Install Doctrine

--- API/config/bundles.php
+++ API/config/bundles.php
@@ -2,5 +2,5 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
 ];
