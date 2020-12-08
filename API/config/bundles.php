3ba647e6d5eaee751559b91918aee74a5f416e43
Dai CAO <56730311+dc4665@users.noreply.github.com>
2020-12-08 23:32:36 +0100

    Add Fixtures Faker and User

--- API/config/bundles.php
+++ API/config/bundles.php
@@ -12,4 +12,5 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
---
+++ API/src/DataFixtures/AppFixtures.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory as Faker;
+use App\Entity\User;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+        $faker = Faker::create('fr_FR');
+        for($i=0 ; $i < 10; $i++) {
+            $user = new User();
+            $user
+                ->setUsername($faker->userName)
+                ->setFirstName($faker->firstName)
+                ->setLastName($faker->lastName)
+                ->setPassword('password')
+                ->setEmail($faker->email)
+                ->setCreatedAt($faker->dateTime)
+            ;
+            $manager->persist($user);
+        }
+        $manager->flush();
+    }
+}
