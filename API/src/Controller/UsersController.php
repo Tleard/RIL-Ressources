d558da8a60424acee5a6bce10205ed71d99e9cf7
Thomas <thomas.leard@gmail.com>
2021-01-11 18:53:37 +0100

    Add User CRUD
    Patch, delete and show routes

--- API/src/Controller/UsersController.php
+++ API/src/Controller/UsersController.php
@@ -6,8 +6,10 @@ namespace App\Controller;
 
 use App\Entity\Asset;
 use App\Entity\User;
+use App\Form\UsersPatchType;
 use App\Form\UsersType;
 use App\Service\FileManager;
+use Doctrine\ORM\NonUniqueResultException;
 use Exception;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
@@ -35,13 +37,11 @@ class UsersController extends AbstractFOSRestController
      */
     public function loginAction(Request $request, JWTTokenManagerInterface $JWTManager, UserPasswordEncoderInterface $encoder)
     {
-        $repository = $this->getDoctrine()->getRepository(User::class);
-
         $em = $this->getDoctrine()->getManager();
         $data = json_decode($request->getContent(), true);
 
         /** @var User $user */
-        $user = $repository->findOneBy(["username" => $data['username']]);
+        $user = $em->getRepository(User::class)->findOneBy(["username" => $data['username']]);
 
         $userPassword = $em->getRepository(User::class)->findOneByUserForPassword($data['username']);
 
@@ -67,7 +67,7 @@ class UsersController extends AbstractFOSRestController
         $data = json_decode($request->getContent(), true);
 
         $user = new User;
-
+        dd($data);
         $em = $this->getDoctrine()->getManager();
         $user->setCreatedAt(new \DateTime());
         //Todo :Improve
@@ -125,9 +125,10 @@ class UsersController extends AbstractFOSRestController
         }
 
     }
+    //Todo : patchUser & DeleteUser
 
     /**
-     * @Route(name="register", path="/api/user/profile_picture/{userId}", methods={"POST"})
+     * @Route(name="add_profile_picture", path="/api/user/profile_picture/{userId}", methods={"POST"})
      * @param Request $request
      * @param FileManager $fileManager
      * @throws Exception
@@ -175,6 +176,120 @@ class UsersController extends AbstractFOSRestController
         $em->flush();
 
         return $this->json($asset, Response::HTTP_CREATED);
+    }
+
+    /**
+     * @Route(name="show_user", path="/api/user/{userId}", methods={"GET"})
+     * @param Request $request
+     * @throws Exception
+     */
+    public function showUserAction(Request $request): JsonResponse
+    {
+        $userId = $request->get('userId');
+        $em = $this->getDoctrine()->getManager();
+
+        try {
+            $user = $em->getRepository(User::class)->find($userId);
+        } catch (NonUniqueResultException $nonUniqueResultException)  {
+            throw new Exception($nonUniqueResultException->getMessage(), Response::HTTP_BAD_REQUEST);
+        }
+
+        if ($user == NULL)
+        {
+            throw new Exception("Could not find user n°" . $userId , Response::HTTP_NOT_FOUND);
+        }
+
+        return $this->json($user, Response::HTTP_FOUND);
+    }
+
+    /**
+     * @Route(name="delete_user", path="/api/user/{userId}", methods={"DELETE"})
+     * @param Request $request
+     * @throws Exception
+     */
+    public function deleteUserAction(Request $request)
+    {
+        $userId = $request->get('userId');
+        $em = $this->getDoctrine()->getManager();
+
+        try {
+            $user = $em->getRepository(User::class)->find($userId);
+        } catch (NonUniqueResultException $nonUniqueResultException)  {
+            throw new Exception($nonUniqueResultException->getMessage(), Response::HTTP_BAD_REQUEST);
+        }
+
+        if ($user == NULL)
+        {
+            throw new Exception("Could not find user n°" . $userId , Response::HTTP_NOT_FOUND);
+        }
+
+        $em->remove($user);
+
+        $this->json('User n°' . $userId . " deleted.");
+    }
+
+    /**
+     * @Route(name="patch_user", path="/api/user/{userId}", methods={"PATCH"})
+     * @param Request $request
+     * @throws Exception
+     */
+    public function patchUserAction(Request $request)
+    {
+        $data = json_decode($request->getContent(), true);
+        $userId = $request->get('userId');
+        $em = $this->getDoctrine()->getManager();
+
+        try {
+            $user = $em->getRepository(User::class)->find($userId);
+        } catch (NonUniqueResultException $nonUniqueResultException)  {
+            throw new Exception($nonUniqueResultException->getMessage(), Response::HTTP_BAD_REQUEST);
+        }
+
+        if ($user == NULL)
+        {
+            throw new Exception("Could not find user n°" . $userId , Response::HTTP_NOT_FOUND);
+        }
+
+        try {
+            //Todo: Pass option to UsersType instead
+            $form = $this->createForm(UsersPatchType::class, $user);
+
+            /** @var Form $form*/
+            $form->submit($data);
+
+            if (!$form->isValid())
+            {
+                $errors = array();
+
+                //Parse errors to print in Json format
+                foreach ($form->getErrors() as $error) {
+                    $errors[$form->getName()][] = $error->getMessage();
+                }
+
+                foreach ($form as $child /** @var Form $child */) {
+                    if (!$child->isValid()) {
+                        foreach ($child->getErrors() as $error) {
+                            $errors[$child->getName()][] = $error->getMessage();
+                        }
+                    }
+                }
+
+                return new JsonResponse([
+                    $errors
+                ], Response::HTTP_BAD_REQUEST);
+            }
+
+            if ($form->isSubmitted())
+            {
+                $em->persist($user);
+                $em->flush();
+            }
+            return $this->json($user, Response::HTTP_CREATED);
+
+            //return $user;
+        } catch (\Exception $exception) {
+            return new JsonResponse(['message' => $exception->getMessage()], Response::HTTP_BAD_REQUEST);
+        }
     }
 
 }
