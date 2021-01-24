72c9e1069a6f19cfe158d36fab3d38f17637c0e1
Thomas <thomas.leard@gmail.com>
2021-01-24 13:05:36 +0100

    Add List Category Route

--- API/src/Controller/ResourcesCategoryController.php
+++ API/src/Controller/ResourcesCategoryController.php
@@ -2,19 +2,41 @@
 
 namespace App\Controller;
 
+use App\Entity\ResourceCategory;
+use Doctrine\ORM\NonUniqueResultException;
+use FOS\RestBundle\View\View as FosRestView;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ResourcesCategoryController extends AbstractController
 {
     /**
-     * @Route("/resources/category", name="resources_category")
+     * @Rest\Get(
+     *     path = "/api/resources_category",
+     *     name = "list_resource_category"
+     * )
+     * @param Request $request
+     * @return FosRestView|Response
      */
-    public function index(): Response
+    public function listResourcesAction(Request $request)
     {
-        return $this->render('resources_category/index.html.twig', [
-            'controller_name' => 'ResourcesCategoryController',
-        ]);
+        $em = $this->getDoctrine()->getManager();
+
+        try {
+            $qb = $em->getRepository(ResourceCategory::class)
+                ->createQueryBuilder('rc');
+
+            $resources = $qb->getQuery()->getResult();
+
+        } catch (NonUniqueResultException $nonUniqueResultException) {
+            return FosRestView::create(['message' => 'Non unique result'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        return $this->json($resources,
+            Response::HTTP_CREATED);
+
     }
 }
