2521c4c8945c4782e0bebcf0560a2a3cc807881d
dc4665 <daihiep.cao@gmail.com>
2021-01-24 23:03:00 +0100

    Reworking navbar - async fetch - test display

---
+++ ressource_relationelle/src/components/AllCategories.js
@@ -0,0 +1,12 @@
+// Block containing all categories. This block contains Categorie component. 
+import React from 'react'
+
+function AllCategories() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default AllCategories
---
+++ ressource_relationelle/src/components/Category.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Category() {
+    
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Category
