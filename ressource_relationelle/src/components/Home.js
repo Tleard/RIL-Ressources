7f2a471f91346e794682cbf1b51b59f2394ba994
dc4665 <daihiep.cao@gmail.com>
2021-02-04 22:03:35 +0100

    Home Page half completed

--- ressource_relationelle/src/components/Home.js
+++ ressource_relationelle/src/components/Home.js
@@ -12,6 +12,7 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid'
 
 // Components import
 import HomeCarousel from "./HomeCarousel";
@@ -59,43 +60,47 @@ function Home() {
     return data;
   };
   //console.log(resources);
-  const mostRecentResources = resources.splice(3, resources.length);
-  console.log(mostRecentResources);
+  const lastResources = resources.slice(0, 3);
+  console.log(lastResources);
   
   return (
     <>
       <HomeCarousel />
-
-
       <Typography variant="h2" component="h2">
         Dernières ressources
       </Typography>
-      <Card className={classes.root}>
-        <CardActionArea>
-          <CardMedia
-            className={classes.media}
-            image="/static/images/cards/contemplative-reptile.jpg"
-            title="Contemplative Reptile"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-              Lizard
-            </Typography>
-            <Typography variant="body2" color="textSecondary" component="p">
-              Lizards are a widespread group of squamate reptiles, with over
-              6,000 species, ranging across all continents except Antarctica
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button size="small" color="primary">
-            Share
-          </Button>
-          <Button size="small" color="primary">
-            Learn More
-          </Button>
-        </CardActions>
-      </Card>
+      <Grid container spacing={5}>
+        {lastResources.map((resource) => (
+          <Grid item xs={12} sm={3} key={resource.id}>
+            <Card className={classes.root}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image="/static/images/cards/contemplative-reptile.jpg"
+                  title="Contemplative Reptile"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {resource.title}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    {resource.description}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button size="small" color="primary">
+                  Consulter
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
     </>
   );
 }
