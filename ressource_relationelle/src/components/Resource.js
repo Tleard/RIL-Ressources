cd9111b95018539b8ed70d67fa33f3b19daed67b
dai4556 <daihiep.cao@gmail.com>
2021-02-13 21:53:38 +0100

    Add Ressource in library fully working

--- ressource_relationelle/src/components/Resource.js
+++ ressource_relationelle/src/components/Resource.js
@@ -1,104 +1,167 @@
 // Component containing a chosen ressource
-import React from 'react'
-import { useState, useEffect } from 'react'
-import "./Resource"
-import auth from '../auth'
-import userlib from '../userLibraryFunctions'
+import React from "react";
+import { useState, useEffect } from "react";
+import "./Resource";
+import auth from "../auth";
+import userlib from "../userLibraryFunctions";
 
 // MaterialUI import
 import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
-import CardHeader from "@material-ui/core/CardHeader"
+import CardHeader from "@material-ui/core/CardHeader";
 import CardContent from "@material-ui/core/CardContent";
-import CardActions from "@material-ui/core/CardActions"
-import IconButton from "@material-ui/core/IconButton"
+import CardActions from "@material-ui/core/CardActions";
+import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import Box from "@material-ui/core/Box";
 
 // MaterialUI Icon Import
-import FavoriteIcon from "@material-ui/icons/Favorite"
-import ShareIcon from "@material-ui/icons/Share"
-import ReportIcon from '@material-ui/icons/Report';
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import ShareIcon from "@material-ui/icons/Share";
+import ReportIcon from "@material-ui/icons/Report";
 
 function Resource(props) {
-    //const {id}  = props.location.state;
-    const id = props.location.hash.substring(1);
+  //const {id}  = props.location.state;
+  const id = props.location.hash.substring(1);
 
-    const [resource, setResource] = useState([]);
+  const [resource, setResource] = useState([]);
+  const [libResources, setLibResources] = useState([]);
 
-    useEffect(() => {
-        if (localStorage.getItem("auth_token")) {
-            const getResource = async () => {
-                const resourceFromServer = await fetchResource();
-                setResource(resourceFromServer);
-            };
+  useEffect(() => {
+    if (localStorage.getItem("auth_token")) {
+      const getResource = async () => {
+        const resourceFromServer = await fetchResource();
+        setResource(resourceFromServer);
+      };
 
-            getResource();
+      const getUserLibrary = async () => {
+        const resourcesFromServer = await fetchLibResources();
+        setLibResources(resourcesFromServer);
+      };
 
-        }
-    }, []);
-
-    // Fetch One Ressource
-    const fetchResource = async () => {
-        const res = await fetch(`${global.api}/api/resources/${id}`, {
-            method: "get",
-            headers: {
-                Accept: "application/json",
-                "Content-type": "application/json",
-                Authorization: `Bearer ${auth.getToken()}`,
-            },
-        })
-        const data = await res.json();
-
-        return data;
+      getResource();
+      getUserLibrary();
     }
+  }, []);
 
-    return (
-      <>
+  // Fetch One Ressource
+  const fetchResource = async () => {
+    const res = await fetch(`${global.api}/api/resources/${id}`, {
+      method: "get",
+      headers: {
+        Accept: "application/json",
+        "Content-type": "application/json",
+        Authorization: `Bearer ${auth.getToken()}`,
+      },
+    });
+    const data = await res.json();
+
+    return data;
+  };
+
+  // Fetch User's saved resources
+  const fetchLibResources = async () => {
+    const res = await fetch(`${global.api}/api/user/getLib`, {
+      method: "post",
+      headers: {
+        Accept: "application/json",
+        "Content-type": "application/json",
+        Authorization: `Bearer ${auth.getToken()}`,
+      },
+    });
+    const data = await res.json();
+
+    return data;
+  };
+
+  // The condition check if the User has something in his library.
+  if (libResources.length > 0) {
+    resource.forEach((resource) => {
+      resource["inLibrary"] = false;
+      for (let libResource of libResources) {
+        if (libResource.id === resource.id) {
+          resource["inLibrary"] = true;
+          break;
+        }
+      }
+    });
+  }
+
+  console.log(resource);
+
+  return (
+    <>
       <Box my={3}>
-        <Typography component="h1" variant="h2" >
+        <Typography component="h1" variant="h2">
           {resource.map((resource) => {
             return (
-              <Typography component="h2" variant="h3">Resource : {resource.title}</Typography>
+              <Typography component="h2" variant="h3">
+                Resource : {resource.title}
+              </Typography>
             );
           })}
         </Typography>
       </Box>
-        {resource.map((resource) => {
-          // To format the Date to dd/mm/YYYY
-          const date = new Date(resource.createdAt);
-          const day = date.getDate();
-          const month = ("0" + date.getMonth()).slice(-1) + 1;
-          const year = date.getFullYear();
-          return (
-            <Card variant="outlined">
-              <CardHeader
-                key={resource.id}
-                title={resource.title}
-                subheader={`${resource.author.username} - ${day}/${month}/${year}`}
-              />
-              <CardContent>{resource.description}</CardContent>
-              <CardActions style={{ justifyContent: "flex-end" }}>
-                <IconButton aria-label="add to favorites">
-                  <FavoriteIcon 
+      {resource.map((resource) => {
+        // To format the Date to dd/mm/YYYY
+        const date = new Date(resource.createdAt);
+        const day = date.getDate();
+        const month = ("0" + date.getMonth()).slice(-1) + 1;
+        const year = date.getFullYear();
+        return (
+          <Card variant="outlined">
+            <CardHeader
+              key={resource.id}
+              title={resource.title}
+              subheader={`${resource.author.username} - ${day}/${month}/${year}`}
+            />
+            <CardContent>{resource.description}</CardContent>
+            <CardActions style={{ justifyContent: "flex-end" }}>
+              <IconButton aria-label="add to favorites">
+                {resource.inLibrary === true ? (
+                  <FavoriteIcon
+                    color="primary"
                     onClick={() => {
-                      userlib.saveInLibrary(resource.id);
+                      userlib.removeFromLibrary(resource.id);
+                      const getResources = async () => {
+                        const libResources = await fetchLibResources();
+                        if (libResources.length > 0) {
+                          setLibResources(libResources);
+                        } else {
+                          setLibResources([0]);
+                        }
+                      };
+
+                      getResources();
                     }}
                   />
-                </IconButton>
-                <IconButton aria-label="share">
-                  <ShareIcon />
-                </IconButton>
-                <IconButton aria-label="report">
-                  <ReportIcon />
-                </IconButton>
-              </CardActions>
-            </Card>
-          );
-        })}
-      </>
-    );
+                ) : (
+                  <FavoriteIcon
+                    onClick={() => {
+                      userlib.saveInLibrary(resource.id);
+                      const getResources = async () => {
+                        const libResources = await fetchLibResources();
+                        setLibResources(libResources);
+                      };
+
+                      getResources();
+                    }}
+                  />
+                )}
+              </IconButton>
+              <IconButton aria-label="share">
+                <ShareIcon />
+              </IconButton>
+              <IconButton aria-label="report">
+                <ReportIcon />
+              </IconButton>
+            </CardActions>
+          </Card>
+        );
+      })}
+    </>
+  );
 }
 
-export default Resource
+export default Resource;
--- ressource_relationelle/src/components/UserLibrary.js
+++ ressource_relationelle/src/components/UserLibrary.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
+import React from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import auth from "../auth";
 import userlib from "../userLibraryFunctions";
@@ -7,131 +7,118 @@ import userlib from "../userLibraryFunctions";
 // MaterialUI import
 import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
-import CardHeader from "@material-ui/core/CardHeader"
+import CardHeader from "@material-ui/core/CardHeader";
 import CardContent from "@material-ui/core/CardContent";
-import CardActions from "@material-ui/core/CardActions"
-import IconButton from "@material-ui/core/IconButton"
+import CardActions from "@material-ui/core/CardActions";
+import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 
-
 // MaterialUI Icon Import
-import FavoriteIcon from "@material-ui/icons/Favorite"
-import ShareIcon from "@material-ui/icons/Share"
-import ReportIcon from '@material-ui/icons/Report';
-import DeleteIcon from '@material-ui/icons/Delete';
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import ShareIcon from "@material-ui/icons/Share";
+import ReportIcon from "@material-ui/icons/Report";
+import DeleteIcon from "@material-ui/icons/Delete";
 
-// MaterialUI Lab 
+// MaterialUI Lab
 import { Alert, AlertTitle } from "@material-ui/lab";
 
 function UserLibrary() {
-    const [libResources, setLibResources] = useState([]);
+  const [libResources, setLibResources] = useState([]);
 
-    useEffect(() => {
-        if (localStorage.getItem("auth_token")) {
-          const getResource = async () => {
-            const resourceFromServer = await fetchResource();
-            setLibResources(resourceFromServer);
-          };
-    
-          getResource();
-          
-        }
-      }, []);
+  useEffect(() => {
+    if (localStorage.getItem("auth_token")) {
+      const getResource = async () => {
+        const resourceFromServer = await fetchResource();
+        setLibResources(resourceFromServer);
+      };
 
-    // Fetch One Ressource
-    const fetchResource = async () => {
-
-        const res = await fetch(`${global.api}/api/user/getLib`, {
-            method: "post",
-            headers: {
-              Accept: "application/json",
-              "Content-type": "application/json",
-              Authorization: `Bearer ${auth.getToken()}`,
-            }
-        })
-        const data = await res.json();
-    
-        return data;
+      getResource();
     }
+  }, []);
 
-    console.log(libResources);
-    console.log(typeof libResources);
+  const fetchResource = async () => {
+    const res = await fetch(`${global.api}/api/user/getLib`, {
+      method: "post",
+      headers: {
+        Accept: "application/json",
+        "Content-type": "application/json",
+        Authorization: `Bearer ${auth.getToken()}`,
+      },
+    });
+    const data = await res.json();
 
-    return (
-      <>
-        <Typography variant="h2" component="h1" style={{ margin: "4% 0 4% 0" }}>
-          Ma Bibliothèque
-        </Typography>
-        <Grid container spacing={3}>
-            {!libResources.message  ?
-              libResources.map((resource) => {
-                // To format the Date to dd/mm/YYYY
-                const date = new Date(resource.createdAt);
-                const day = date.getDate();
-                const month = ("0" + date.getMonth()).slice(-1) + 1;
-                const year = date.getFullYear();
+    return data;
+  };
 
-                return (
-                  <Grid item xs={12}>
-                    <Card variant="outlined">
-                      <CardHeader
-                        title={resource.title}
-                        subheader={`${resource.author.username} - ${day}/${month}/${year}`}
+  return (
+    <>
+      <Typography variant="h2" component="h1" style={{ margin: "4% 0 4% 0" }}>
+        Ma Bibliothèque
+      </Typography>
+      <Grid container spacing={3}>
+        {!libResources.message ? (
+          libResources.map((resource) => {
+            // To format the Date to dd/mm/YYYY
+            const date = new Date(resource.createdAt);
+            const day = date.getDate();
+            const month = ("0" + date.getMonth()).slice(-1) + 1;
+            const year = date.getFullYear();
+
+            return (
+              <Grid item xs={12}>
+                <Card variant="outlined">
+                  <CardHeader
+                    title={resource.title}
+                    subheader={`${resource.author.username} - ${day}/${month}/${year}`}
+                  />
+                  <CardContent>{resource.description}</CardContent>
+                  <CardActions style={{ justifyContent: "flex-end" }}>
+                    <IconButton aria-label="share">
+                      <DeleteIcon
+                        onClick={() => {
+                          userlib.removeFromLibrary(resource.id);
+                          const getResource = async () => {
+                            const resourceFromServer = await fetchResource();
+                            setLibResources(resourceFromServer);
+                          };
+
+                          getResource();
+                        }}
                       />
-                      <CardContent>{resource.description}</CardContent>
-                      <CardActions style={{ justifyContent: "flex-end" }}>
-                        <IconButton aria-label="add to favorites">
-                          <FavoriteIcon
-                            onClick={() => {
-                              userlib.saveInLibrary(resource.id);
-                            }}
-                          />
-                        </IconButton>
-                        <IconButton aria-label="share">
-                          <DeleteIcon
-                            onClick={() => {
-                              userlib.removeFromLibrary(resource.id);
-                              const getResource = async () => {
-                                const resourceFromServer = await fetchResource();
-                                setLibResources(resourceFromServer);
-                              };
-
-                              getResource();
-                            }}
-                          />
-                        </IconButton>
-                        <IconButton aria-label="share">
-                          <ShareIcon />
-                        </IconButton>
-                        <IconButton aria-label="report">
-                          <ReportIcon />
-                        </IconButton>
-                        <Button size="medium" color="primary">
-                          <Link
-                            key={resource.id}
-                            //to={{pathname: "category/resource", state: {id: resource.id}}}
-                            to={{
-                              pathname: "resource",
-                              hash: `${resource.id}`,
-                            }}
-                          >
-                            Consulter
-                          </Link>
-                        </Button>
-                      </CardActions>
-                    </Card>
-                  </Grid>
-                );
-              })
-              : <Alert severity="info">
-              <AlertTitle>Attention</AlertTitle>
-              Votre bibliothèque est vide
-            </Alert>
-          }
-        </Grid>
-      </>
-    );
+                    </IconButton>
+                    <IconButton aria-label="share">
+                      <ShareIcon />
+                    </IconButton>
+                    <IconButton aria-label="report">
+                      <ReportIcon />
+                    </IconButton>
+                    <Button size="medium" color="primary">
+                      <Link
+                        key={resource.id}
+                        //to={{pathname: "category/resource", state: {id: resource.id}}}
+                        to={{
+                          pathname: "resource",
+                          hash: `${resource.id}`,
+                        }}
+                      >
+                        Consulter
+                      </Link>
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            );
+          })
+        ) : (
+          <Alert severity="info">
+            <AlertTitle>Attention</AlertTitle>
+            Votre bibliothèque est vide
+          </Alert>
+        )}
+      </Grid>
+    </>
+  );
 }
 
-export default UserLibrary
+export default UserLibrary;
